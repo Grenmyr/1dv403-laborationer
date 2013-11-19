2cb9eb00e48bb6455388e787117940dbf32a891c
OpenShift guest <dg222cs@student.lnu.se>
2013-11-19 09:29:29 -0500

    arbetat vidare med labben, snart klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,25 +1,38 @@
 "use strict";
 
 var makePerson = function(persArr){
-	var maxAge;
-    var minAge;
-    var personAges;
-    var averageAge;
+    var personArray;
+    var personNamesArray;
 	var myPersonObject = {};
-	
-	
-	
-	
-    var personArray = persArr.map(function(person){
+	var averageAge;
+    
+    /* Array som skapas och hämtar ut alla  "age" element från vandalen.js 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map */
+    
+    personArray = persArr.map(function(person){
         return  person.age;
     });
+     /* Array array som skapas för att kunna hantera alla "name element"*/
+     personNamesArray = persArr.map(function(person){
+        return  person.name;
+    });
     
+    /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce */
     console.log(personArray); 
     myPersonObject.minAge = personArray.reduce(function(previousValue, currentValue, index, personArray){
         return Math.min(previousValue, currentValue);
     });
     
-    //myObj.maxAge = maxage;
+    myPersonObject.maxAge = personArray.reduce(function(previousValue, currentValue, index, personArray){
+        return Math.max(previousValue, currentValue);
+    });
+    
+    averageAge  = personArray.reduce(function(age, index){
+        
+        return (age+index);
+    });
+    myPersonObject.averageAge = 5;
+    
 	return myPersonObject;
 	// Din kod här..retun person.
 
