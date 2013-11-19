67077dc491908e0bb80df4dd7f1f3ae88a8be7bc
OpenShift guest <dg222cs@student.lnu.se>
2013-11-19 10:47:25 -0500

    klar med uppgift vandalen

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -15,7 +15,7 @@ var makePerson = function(persArr){
     });
     
     /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce */
-    console.log(personNamesArray); 
+    
     myPersonObject.minAge = personArray.reduce(function(previousValue, currentValue, index, personArray){
         return Math.min(previousValue, currentValue);
     });
@@ -35,11 +35,19 @@ var makePerson = function(persArr){
     
      // HÄREFTER HANTERAS EGENSKAPERNA AV TYP "name"
      
-     /* Array skapas/hämtas från testet för att kunna hantera alla "name element"*/
-     personNamesArray = persArr.map(function(person){
-        return  person.name;
+     // Array skapas/hämtas från testet för att kunna hantera alla "name element"*/
+    personNamesArray = persArr.map(function(person){
+        return person.name ;
     });
     
+    // Sort är en anonym funktion som sorterar på alla möjliga tecken inklusive åäö.
+    personNamesArray.sort(function(a, b){
+        return a.localeCompare(b);
+    });
+    
+    myPersonObject.names = personNamesArray.reduce(function(previousName, name, index, personNamesArray){
+        return previousName + ", " + name;
+    });
     
     
 	return myPersonObject;
