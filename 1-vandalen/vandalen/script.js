1c4cc46afae535fa97d810e77cb47487437d27d4
OpenShift guest <dg222cs@student.lnu.se>
2013-11-19 08:57:33 -0500

    ojoj tungt o svårt men tillsammans är vi starka

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -10,12 +10,14 @@ var makePerson = function(persArr){
 	
 	
 	
-    personAges = persArr.map(function(person){
-        return person.age;
+    var personArray = persArr.map(function(person){
+        return  person.age;
     });
     
-    console.log(personAges); 
-    minAge = 
+    console.log(personArray); 
+    myPersonObject.minAge = personArray.reduce(function(previousValue, currentValue, index, personArray){
+        return Math.min(previousValue, currentValue);
+    });
     
     //myObj.maxAge = maxage;
 	return myPersonObject;
