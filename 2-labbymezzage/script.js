09040781ff3399d2f39e75e1629c2419cca4efc7
OpenShift guest <dg222cs@student.lnu.se>
2013-11-27 04:56:26 -0500

    uppdate

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -1,13 +1,24 @@
 window.onload = function (){
-    var mess = new Message("tjohej");
     var Messageboard = {
-        
         messagesArray: []
     };
-    Messageboard.messagesArray.push("Detta är","en fis", "påge");    
     
-    console.log(Messageboard.messagesArray);
-    console.log(mess.getText(Messageboard.messagesArray[1]),mess.getDate(Messageboard.messagesArray[1]));
+    
+    var strText = 'hej hej';
+    var mess1 = new Message(strText, new Date());
+    
+    Messageboard.messagesArray.push(mess1);
+    
+    /*
+    var a = Messageboard.messagesArray;
+    for(var i = 0; i < a.length; i++){
+        console.log(a[i].getText());    
+    }
+    */
+    
+    
+    //console.log(Messageboard.messagesArray);
+    //console.log(mess.getText(Messageboard.messagesArray[1]),mess.getDate(Messageboard.messagesArray[1]));
     
     /*var mess = new Message("olle hade en stor dase", new Date());
     alert(mess);
