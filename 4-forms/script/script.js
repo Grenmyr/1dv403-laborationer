eabd7c036da3ea7a38761aa0af4347c59e05863e
OpenShift guest <dg222cs@student.lnu.se>
2013-12-16 07:38:42 -0500

    set up fortfarande

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -1,18 +1,27 @@
 "use strict";
 var FormApp = {
+    Init : function(){
+        FormApp.OnSubmit();
+    },
     OnSubmit : function (){
+        // när användaren trycker på skicka knappen trigga denna funktion.
+        // kontrollera då formuläret, att det är godkänt, annars false.
         var form = document.getElementById("form");
-        
+        form.onsubmit = function(e) {
+            alert("va?");
+            //form.elements["text"];
+            //console.log(form.elements["text"]);
+        };
         if(!true){
             return false;
         }
     },
     OnReset : function (){},
-    Reset : function (){},
+    Reset : function (){/* töm formuläret med denna efter gokänd data*/},
     Submit : function (){}
     };
 
 window.onload = function(){
-FormApp.OnSubmit();
+FormApp.Init();
  
 };
