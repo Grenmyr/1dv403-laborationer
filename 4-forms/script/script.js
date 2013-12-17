3dd6c34cfb78a671350185f1f5643501336c26c0
OpenShift guest <dg222cs@student.lnu.se>
2013-12-17 04:09:11 -0500

    börjat

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -2,20 +2,41 @@
 var FormApp = {
     Init : function(){
         FormApp.OnSubmit();
+         
     },
     OnSubmit : function (){
         // när användaren trycker på skicka knappen trigga denna funktion.
         // kontrollera då formuläret, att det är godkänt, annars false.
         var form = document.getElementById("form");
         form.onsubmit = function(e) {
-            alert("va?");
-            //form.elements["text"];
-            //console.log(form.elements["text"]);
+          };
+        var fName = document.getElementById("lastName");
+        fName.onblur = function(){
+              if(fName.value === "" || fName.value === null ){
+              var lnDiv = document.getElementById("lnDiv");
+              var text = document.createTextNode("Du har glömt fylla i formulärdata");
+              var newDiv = document.createElement("div");
+              
+              newDiv.appendChild(text);   
+              lnDiv.appendChild(newDiv);
+            }  
+            
         };
-        if(!true){
-            return false;
-        }
+        var lName = document.getElementById("firstName");
+        lName.onblur = function(){
+              if( lName.value === "" ||  lName.value === null ){
+              var fnDiv = document.getElementById("fnDiv");
+              var text = document.createTextNode("Du har glömt fylla i formulärdata");
+              var newDiv = document.createElement("div");
+              
+              newDiv.appendChild(text);   
+              fnDiv.appendChild(newDiv);
+            }  
+            
+        };
+        
     },
+    Blur : function (){},
     OnReset : function (){},
     Reset : function (){/* töm formuläret med denna efter gokänd data*/},
     Submit : function (){}
