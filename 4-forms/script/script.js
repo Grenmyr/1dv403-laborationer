8bc2a4af35b1245d1487959188173f9027c5c657
OpenShift guest <dg222cs@student.lnu.se>
2013-12-17 19:29:27 -0500

    cp labb johan skärp dig

--- 4-forms/script/script.js
+++ 4-forms/script/script.js
@@ -2,33 +2,30 @@
 var FormApp = {
     Init : function(){
         FormApp.Submit();
+        FormApp.Validator(); 
     },
     Trigger1 : false,
     Trigger2 : false,
     Trigger3 : false,
     Trigger4 : false,
-    olle : 0,
-    WrongNotDone : false,
     MsgExist: null,
-    
     Submit : function (){
         var button = document.getElementById("button");
         
         button.addEventListener("click", function(){
                 FormApp.Blur();
         },false);
-        
     },
     
     Validator : function (){
         
         var fName = document.getElementById("firstName");
         fName.onblur = function(){
-               
               if(fName.value === "" || fName.value === null){
                 FormApp.Trigger1 = false;
                 if(!this.MsgExist){
                     FormApp.SetError(1, "fnDiv");
+                    this.MsgExist=true;
               }
             } 
             else{
@@ -46,7 +43,6 @@ var FormApp = {
                 if(!this.MsgExist){
                     FormApp.SetError(2, "lnDiv");
                     this.MsgExist=true;
-                    
                 }
             }
              else{
@@ -55,34 +51,26 @@ var FormApp = {
                 tag.parentNode.removeChild(tag);
                 this.MsgExist = null;
             }
-          
         };
         var eMail = document.getElementById("eMail");
         eMail.onblur = function (){
-            
             
             if(!eMail.value.match(/^[a-zA-Z0-9.!#$%&\'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)|| eMail.value === ""){
                 if(!this.MsgExist){
                     FormApp.Trigger3 = false;
                     FormApp.SetError(3, "eMailDiv");
                     this.MsgExist=true;
-                    
                 } 
             }
             else{
                 FormApp.Trigger3 = true; 
                 var tag = document.getElementById("id3");
                 tag.parentNode.removeChild(tag);
-                
             }
-          
         };
         var zipCode = document.getElementById("zipCode");
         zipCode.onblur = function (){
-            
-            
             var zipS = zipCode.value;
-            
             if(zipS.match(/^\d{5}$/) || zipS.match(/^\d{3}[- ]\d{2}$/) || zipS.match(/^[SE]+\d{5}$/)|| zipS.match(/^[SE]+\d{3}[- ]\d{2}$/)|| zipS.match(/^[SE ]+(\d{3}[- ]\d{2}|\d{5})$/)){
                FormApp.Trigger4 = true;
                zipS=zipS.replace(/-/g,"");
@@ -99,12 +87,7 @@ var FormApp = {
                 this.MsgExist=true;
                 } 
             } 
-            
-                
-            
         };
-        
-        
     },
     SetError: function(idNumber, divName){
         var div = divName;
@@ -115,33 +98,22 @@ var FormApp = {
         small.setAttribute("class", "error");
         small.appendChild(text);
         div.appendChild(small);
-        
-        
-        FormApp.WrongNotDone === true;
     },
     Blur : function (){
         FormApp.Validator(); 
-        console.log(FormApp.WrongNotDone+"  här loggar jag notdone");
-        var errorMsg = document.querySelectorAll(".error");
-        console.log(errorMsg);
         console.log(FormApp.Trigger1+"  loggar trigger1");
         console.log(FormApp.Trigger2+"  loggar trigger2");
         console.log(FormApp.Trigger3+"  loggar trigger3");
         console.log(FormApp.Trigger4+"  loggar trigger4");
-        
-        console.log("här loggar jag errorlength"+errorMsg.length);
+
         if(FormApp.Trigger1 === true && FormApp.Trigger2 === true && FormApp.Trigger3 === true && FormApp.Trigger4 === true ){
             console.log("vill ej komma in här");
-          
         }
-        console.log("success");
     },
     OnReset : function (){
     },
     Reset : function (){/* töm formuläret med denna efter gokänd data*/
-    
     }
-   
     };
 
 window.onload = function(){
