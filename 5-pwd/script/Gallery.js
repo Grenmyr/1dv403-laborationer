66970cab58ed85d3630ffa9f25503455bdd647e3
Grenmyr <dg222cs@student.lnu.se>
2014-01-02 20:41:49 +0100

    fasdfdsa

--- 5-pwd/script/Gallery.js
+++ 5-pwd/script/Gallery.js
@@ -1,25 +1,12 @@
 "use strict";
 var Gallery = function () {
-    var xhr = new XMLHttpRequest()
+    
     this.init = function (windowID) {
         var aside = document.getElementById("aside" + windowID);
         aside.innerHTML = "testar bara att skiten funkar!!!!";
         var xhr = new XMLHttpRequest()
+        console.log(xhr);
         xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
-        xhr.onreadystatechange = function () {
-            xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
-            if (xhr.readyState === 4) {
-                alert(xhr.readyState);
-            }
-            
-            console.log("dasdsa" + xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true));
-        }
-
-        
-        
-
-        console.log(xhr)
-        var dsadas = "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/";
-       
+        console.log(xhr);
     }
 }
