fb999c49efece5cd52f3f9e5e1dc4aa41370c807
Grenmyr <dg222cs@student.lnu.se>
2014-01-03 00:07:53 +0100

    det var inte firefox fel, det var mongo c9.io som inte funkade med firefox o hämta xml request

--- 5-pwd/script/Gallery.js
+++ 5-pwd/script/Gallery.js
@@ -1,26 +1,51 @@
 "use strict";
-var Gallery = function () {
+var Gallery = function (_xhr) {
+    
     
     this.init = function (windowID) {
         var aside = document.getElementById("aside" + windowID);
         aside.innerHTML = "testar bara att skiten funkar!!!!";
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function (){
-            if(xhr.readyState === 4){
-                if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304){
+        //var ddd = new XMLHttpRequest();
+        this.setXhr();
+        console.log(this.getXhr());
+        this.rState();  
+       
+        
+        this.getXhr().open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
+        this.getXhr().send(null);
+        
+     
+       
+        
+        //var json = JSON.parse("get","http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/pics",true);
+       // console.log(json);
+       
+    };
+    this.rState = function () {
+        var xhr = this.getXhr();
+        xhr.onreadystatechange = function () {
+
+            if (xhr.readyState === 4) {
+
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
                     alert(xhr.responseText);
+                    var response = xhr.responseText;
+                    return xhr.responseText;
                 }
                 else {
                     console.log("fell");
                 }
             }
         };
-        xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
-        xhr.send(null);
-       
         
-        //var json = JSON.parse("get","http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/pics",true);
-       // console.log(json);
-       
+
+
+    }
+    this.getXhr = function () {
+        return _xhr;
+    }
+    this.setXhr = function () {
+        _xhr = new XMLHttpRequest();
+        console.log(_xhr);
     };
 };
