13eda4b74c2f348806e0b05f541d0e05c668bd76
OpenShift guest <dg222cs@student.lnu.se>
2014-01-02 15:57:55 -0500

    skitfirefox

--- 5-pwd/script/Gallery.js
+++ 5-pwd/script/Gallery.js
@@ -4,9 +4,23 @@ var Gallery = function () {
     this.init = function (windowID) {
         var aside = document.getElementById("aside" + windowID);
         aside.innerHTML = "testar bara att skiten funkar!!!!";
-        var xhr = new XMLHttpRequest()
-        console.log(xhr);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState === 4){
+                if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304){
+                    alert(xhr.responseText);
+                }
+                else {
+                    console.log("fell");
+                }
+            }
+        };
         xhr.open("get", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
-        console.log(xhr);
-    }
-}
+        xhr.send(null);
+       
+        
+        //var json = JSON.parse("get","http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/pics",true);
+       // console.log(json);
+       
+    };
+};
