c096c92a189903bc02ed2eb4721f72adc3bf430f
Grenmyr <dg222cs@student.lnu.se>
2014-01-09 13:10:41 +0100

    jobbat med gränser i drag funktionen

--- 5-pwd/script/Window.js
+++ 5-pwd/script/Window.js
@@ -32,21 +32,27 @@ function MyWindow(windowId, position) {
     article.appendChild(aside);
     footer.appendChild(ptag);
     article.appendChild(footer);
-
+    
     article.onmousedown = function () {
         var all = document.querySelectorAll(".article")
         for (var i = 0; i < all.length; i++) {
             all[i].style.zIndex = 1;
         }
         article.style.zIndex = 999;
-        var drag = new DragDrop();
+        DragDrop.enable();
+        
     };
+    article.onmouseup = function () {
+        console.log(DragDrop);
+        DragDrop.disable();
+        console.log(DragDrop);
+    }
     exitButton.onclick = function () {
         Portal.onClosedWindow();
         article.parentElement.removeChild(article);
 
-        
-        
+
+
 
     };
     this.setWindowForImageView = function (Jsonobject) {
@@ -58,34 +64,25 @@ function MyWindow(windowId, position) {
         article.style.width = width + "px";
         article.style.top = 25 + "px";
         article.style.left = 500 + "px";
+        return [height, width];
     };
 
     // public interface
 
-    
-    
-};
-/*disable : function () {
-    document.removeHandler("mousedown", handleEvent, false);
-    document.removeHandler("mousemove", handleEvent, false);
-    document.removeHandler("mouseup", handleEvent, false);
-}*/
 
-function DragDrop() {
+
+};
+
+
+var DragDrop = function () {
 
     var dragging = null,
         // initialize variables used later for checking difference in mouse and target position.
         diffX = 0,
         diffY = 0;
-    
-        document.addEventListener("mousedown", handleEvent, false);
-        document.addEventListener("mousemove", handleEvent, false);
-        document.addEventListener("mouseup", handleEvent, false);
 
-       
-    
     function handleEvent(event) {
-       
+
 
         var target = event.target;
         //determine the type of event
@@ -101,11 +98,32 @@ function DragDrop() {
 
             case "mousemove":
                 if (dragging !== null) {
-                    //assign location
+                    //assign location (window.innerwidth)
+                    if ((event.clientY -diffY) < 0) {
+                        dragging.parentNode.style.top = 0 + "px";
+                        break;
+                    }
+                    if ((event.clientY -diffY)> 400) {
+                        console.log(event.clientY - diffY);
+                        dragging.parentNode.style.top = 400 + "px";
+                        break;
+                    }
+                    if ((event.clientX - diffX) < 0) {
+
+                        dragging.parentNode.style.left = 0 + "px";
+                        break;
+                    }
+                    if ((event.clientX - diffX) > 1345) {
+                        dragging.parentNode.style.left = 1345 + "px";
+                        break;
+                    }
+
                     dragging.parentNode.style.left = (event.clientX - diffX) + "px";
                     dragging.parentNode.style.top = (event.clientY - diffY) + "px";
 
+
                 }
+
                 break;
 
             case "mouseup":
@@ -113,8 +131,22 @@ function DragDrop() {
 
                 break;
         }
+    };
+
+    return {
+        enable: function () {
+            document.addEventListener("mousedown", handleEvent, false);
+            document.addEventListener("mousemove", handleEvent, false);
+            document.addEventListener("mouseup", handleEvent, false);
+        },
+
+        disable: function () {
+            document.removeEventListener("mousedown", handleEvent, false);
+            document.removeEventListener("mousemove", handleEvent, false);
+            document.removeEventListener("mouseup", handleEvent, false);
+        }
     }
-};
+}();
 
 /*var td = document.createElement("td");
 var a = document.createElement("a");
